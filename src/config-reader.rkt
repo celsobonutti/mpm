593e267f49c70008cb5764076363612b2b0f9c42
Celso Bonutti <i.am@cel.so>
2022-11-04 19:43:36 -0300

    refactor versoin

--- src/config-reader.rkt
+++ src/config-reader.rkt
@@ -7,7 +7,7 @@
   #:guard (λ(name file-list version# type-name)
             (let ([valid-file-list (file-list? file-list)]
                   [valid-name      (name? name)]
-                  [valid-version#  (unknown->version# version#)])
+                  [valid-version#  (make-version version#)])
               (cond [(not valid-file-list) (error "File list is not a list")]
                     [(not valid-name)      (error "Invalid name for package")]
                     [(not valid-version#)  (error "Invalid version number")]
@@ -50,12 +50,12 @@
 
 ;; Validate version number
 
-(define (unknown->version# unk)
+(define (make-version unk)
   (define (get-parts str)
     (match (map string->number (string-split str "."))
-      [(list major) (version# major 0 0)]
-      [(list major minor) (version# major minor 0)]
-      [(list major minor patch) (version# major minor patch)]
+      [(list major) (version major 0 0)]
+      [(list major minor) (version major minor 0)]
+      [(list major minor patch) (version major minor patch)]
       [else #f]))
 
   (cond [(string? unk) (get-parts unk)]
@@ -63,26 +63,17 @@
         [else (error "Invalid value for version#")]))
 
 
-(struct version# (major minor patch)
+(struct version (major minor patch)
   #:guard (λ(major minor patch type-name)
             (cond [(not (integer? major)) (error "Major is not an integer")]
                   [(not (integer? minor)) (error "Minor is not an integer")]
                   [(not (integer? patch)) (error "Patch is not an integer")]
-                  [else (values major minor patch)]))
-  #:methods
-  gen:equal+hash
-  [(define (equal-proc a b equal?-recur)
-     (and (equal?-recur (version#-major a) (version#-major b))
-          (equal?-recur (version#-minor a) (version#-minor b))
-          (equal?-recur (version#-patch a) (version#-patch b))))
-   (define (hash-proc a hash-recur)
-     (+ (hash-recur (version#-major a))
-        (* 3 (hash-recur (version#-minor a)))
-        (* 9 (hash-recur (version#-patch a)))))
-   (define (hash2-proc a hash2-recur)
-     (+ (hash2-recur (version#-major a))
-        (hash2-recur (version#-minor a))
-        (hash2-recur (version#-patch a))0))])
+                  [else (values major minor patch)])))
+
+(define (version=? v1 v2)
+  (and (= (version-major v1) (version-major v2))
+       (= (version-minor v1) (version-minor v2))
+       (= (version-patch v1) (version-patch v2))))
 
 
 ;; Reader
@@ -108,10 +99,10 @@
 (check-exn exn:fail? (thunk (valid-package-name? "Memes")))
 (check-exn exn:fail? (thunk (valid-package-name? "-oof")))
 
-(check-true (version#? (unknown->version# "1.1.0")))
-(check-true (version#? (unknown->version# '1.1.0)))
-(check-true (version#? (unknown->version# "1")))
-(check-true (version#? (unknown->version# "1.1")))
+(check-true (version? (make-version "1.1.0")))
+(check-true (version? (make-version '1.1.0)))
+(check-true (version? (make-version "1")))
+(check-true (version? (make-version "1.1")))
 
 (check-true (package? (package 'my-package '() '1.0.0)))
 
@@ -119,7 +110,7 @@
 (check-true (let* ([stdlib  (read-config "../example/test.mpm")]
                    [name    (package-name stdlib)]
                    [files   (package-file-list stdlib)]
-                   [version (package-version# stdlib)])
+                   [version# (package-version# stdlib)])
               (and (string=? name "std")
                    (null? files)
-                   (equal? version (version# 1 0 0)))))
+                   (version=? version# (version 1 0 0)))))
